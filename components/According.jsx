81f789a844b51c2a9c06c3f581409fa1328a7ea3
Mohith kumar <murugan28aug@gmail.com>
2023-07-03 23:43:29 +0530

    hero

--- components/According.jsx
+++ components/According.jsx
@@ -33,7 +33,7 @@ const faq=[
         <Disclosure key={i.question}>
           {({ open }) => (
             <>
-              <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-lg font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+              <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-base md:text-lg font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
                 <span>{i.question}</span>
                 <ChevronUpIcon
                   className={`${
