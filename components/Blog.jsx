b004fe8741960aa713c70d433fdc2b9f02c4712c
Mohith kumar <murugan28aug@gmail.com>
2023-07-06 04:16:31 +0530

    fina;

--- components/Blog.jsx
+++ components/Blog.jsx
@@ -1,9 +1,10 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Blog = () => {
   return (
     <div>
-        <section className="text-gray-600 body-font bg-white pb-44">
+        <section className="text-gray-600 body-font bg-white pb-44 relative z-10">
   <div className="container px-5 py-24 mx-auto max-w-6xl">
   <div className="flex flex-col">
       <div className="h-1 bg-gray-800 rounded overflow-hidden">
@@ -20,6 +21,8 @@ const Blog = () => {
 
 
     <div className="flex flex-wrap -mx-4 -my-8">
+      
+            
       <div className="py-8 px-4 lg:w-1/3">
         <div className="h-full flex items-start">
           <div className="w-12 flex-shrink-0 flex flex-col text-center leading-none">
@@ -27,9 +30,9 @@ const Blog = () => {
             <span className="font-medium text-lg text-gray-800 title-font leading-none">18</span>
           </div>
           <div className="flex-grow pl-6">
-            <h2 className="tracking-widest text-xs title-font font-medium text-purple-500 mb-1">PRODUCTIVITY</h2>
-            <h1 className="title-font text-xl font-medium text-gray-900 mb-3">Mastering Remote Work Productivity: Tips and Strategies</h1>
-            <p className="leading-relaxed mb-5"> This blog explores various strategies and tips to enhance productivity while working remotely. From creating a dedicated workspace to managing time effectively, discover practical techniques to stay focused, motivated, and productive in a remote work setup.</p>
+            <h2 className="tracking-widest text-xs title-font font-medium text-purple-500 mb-1">Unveiling the Power of Boujee:</h2>
+            <h1 className="title-font text-xl font-medium text-gray-900 mb-3">   <a target='_blank' href='https://www.bannerbank.com/financial-resources/blog/tips-to-prevent-online-fraud'>Protecting Against Online Fraud  </a></h1>
+            <p className="leading-relaxed mb-5"> In today's digital landscape, online fraud has become a pervasive threat that affects individuals and businesses alike. The evolving tactics used by fraudsters make it challenging for organizations to detect and prevent fraudulent activity effectively. That's where Boujee comes in.</p>
             <a className="inline-flex items-center">
               <img alt="blog" src="https://dummyimage.com/103x103" className="w-8 h-8 rounded-full flex-shrink-0 object-cover object-center"/>
               <span className="flex-grow flex flex-col pl-3">
@@ -39,6 +42,8 @@ const Blog = () => {
           </div>
         </div>
       </div>
+    
+      
       <div className="py-8 px-4 lg:w-1/3">
         <div className="h-full flex items-start">
           <div className="w-12 flex-shrink-0 flex flex-col text-center leading-none">
@@ -46,9 +51,9 @@ const Blog = () => {
             <span className="font-medium text-lg text-gray-800 title-font leading-none">30</span>
           </div>
           <div className="flex-grow pl-6">
-            <h2 className="tracking-widest text-xs title-font font-medium text-purple-500 mb-1">E-COMMERCE</h2>
-            <h1 className="title-font text-xl font-medium text-gray-900 mb-3">The Future of E-commerce: Trends and Innovations to Watch</h1>
-            <p className="leading-relaxed mb-5"> Dive into the evolving landscape of e-commerce and discover the latest trends and innovations shaping its future. This blog highlights advancements such as personalized shopping experiences, voice commerce, augmented reality, and more.</p>
+            <h2 className="tracking-widest text-xs title-font font-medium text-purple-500 mb-1">Enhancing Security with Boujee:</h2>
+            <h1 className="title-font text-xl font-medium text-gray-900 mb-3"> <a target='_blank' href='https://www.bannerbank.com/financial-resources/blog/tips-to-prevent-online-fraud'>Defending Against Digital Risks</a></h1>
+            <p className="leading-relaxed mb-5">In the digital age, organizations face a multitude of digital risks that threaten the security of their sensitive information. Data breaches, identity theft, and cyber attacks have become all too common, and traditional security measures often fall short in effectively countering these threats. That's where Boujee steps in.</p>
             <a className="inline-flex items-center">
               <img alt="blog" src="https://dummyimage.com/102x102" className="w-8 h-8 rounded-full flex-shrink-0 object-cover object-center"/>
               <span className="flex-grow flex flex-col pl-3">
@@ -58,6 +63,8 @@ const Blog = () => {
           </div>
         </div>
       </div>
+
+      
       <div className="py-8 px-4 lg:w-1/3">
         <div className="h-full flex items-start">
           <div className="w-12 flex-shrink-0 flex flex-col text-center leading-none">
@@ -65,9 +72,9 @@ const Blog = () => {
             <span className="font-medium text-lg text-gray-800 title-font leading-none">06</span>
           </div>
           <div className="flex-grow pl-6">
-            <h2 className="tracking-widest text-xs title-font font-medium text-purple-500 mb-1">COMMUNICATION</h2>
-            <h1 className="title-font text-xl font-medium text-gray-900 mb-3">The Art of Effective Communication: Building Stronger Connections</h1>
-            <p className="leading-relaxed mb-5">Effective communication is key to building strong relationships, both personally and professionally. In this blog, learn valuable techniques to improve your communication skills, including active listening, non-verbal cues, and clear articulation.</p>
+            <h2 className="tracking-widest text-xs title-font font-medium text-purple-500 mb-1">Unmasking the Power of Boujee: </h2>
+            <h1 className="title-font text-xl font-medium text-gray-900 mb-3"><a target='_blank' href='https://www.bannerbank.com/financial-resources/blog/tips-to-prevent-online-fraud'>Combatting Sophisticated Attacks</a></h1> 
+            <p className="leading-relaxed mb-5">Cybercriminals are continually developing sophisticated attack techniques to breach organizational defenses and compromise sensitive data. Traditional security measures are often insufficient in countering these advanced threats, leaving organizations vulnerable to significant financial and reputational damage.</p>
             <a className="inline-flex items-center">
               <img alt="blog" src="https://dummyimage.com/101x101" className="w-8 h-8 rounded-full flex-shrink-0 object-cover object-center"/>
               <span className="flex-grow flex flex-col pl-3">
@@ -77,6 +84,7 @@ const Blog = () => {
           </div>
         </div>
       </div>
+      
     </div>
   </div>
 </section>
