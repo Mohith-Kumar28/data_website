bbd1e6adc894b3f8cb4a589b134b6056d7386002
Mohith kumar <murugan28aug@gmail.com>
2023-07-06 10:26:18 +0530

    ui

--- components/Faq.jsx
+++ components/Faq.jsx
@@ -18,7 +18,7 @@ const Faq = () => {
   <div className="  container max-w-5xl px-5 py-24 md:mt-10 md:pb-32 mx-auto flex ">
     <h2 className="sm:text-3xl text-2xl text-white font-medium title-font mb-2 ">FAQ's</h2>
     <div className="flex flex-grow ">
-    <div className='w-3   rounded-full  h-full flex-col md:bg-gray-800 mx-4'></div>
+    <div className='w-3 relative z-30  rounded-full  h-full flex-col md:bg-gray-800 mx-4'></div>
      <According/>
       
     </div>
